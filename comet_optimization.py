29efaaa71213ba1cafebdb413bfa9366fe4270e3
Attila <attila.simko@umu.se>
2023-03-01 06:20:26 +0100

    go

--- comet_optimization.py
+++ comet_optimization.py
@@ -2,40 +2,31 @@ import comet_ml
 import models
 import utils
 import argparse
-
+import os
 comet_ml.init(api_key="ro9UfCMFS2O73enclmXbXfJJj", project_name='comet-optimizer')
 
 parser = argparse.ArgumentParser(description='Welcome.')
-parser.add_argument("--server", default=None) # sct / denoise / segment
+parser.add_argument("--gpu", default=None) # sct / denoise / segment
 parser.add_argument("--task", default="sct") # sct / denoise / segment
 parser.add_argument("--model", default="srresnet") # srresnet / pix2pix / diffusion
 args = parser.parse_args()
+
+if args.gpu is not None:
+    os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+    os.environ["CUDA_VISIBLE_DEVICES"] = str(args.gpu)
 
 if (args.model == "srresnet"):
     config = models.SRResNet.config
 opt = comet_ml.Optimizer(config)
 
-if (args.server is None):
-    if (args.task == "sct"):
-        data_path = '/mnt/4a39cb60-7f1f-4651-81cb-029245d590eb/DS0060/'
-    else:
-        raise Exception("Unknown task")
-elif (args.server == "laplace"):
-    if (args.task == "sct"):
-        data_path = '/data/attila/DS0060/'
-    else:
-        raise Exception("Unknown task")
-
-
 experiment_idx = 0
 for experiment in opt.get_experiments():
     experiment.set_name(f"{args.task}_{args.model}_{experiment_idx}")
     experiment_idx += 1
-    experiment.log_parameter("server", args.server)
     experiment.log_parameter("task", args.task)
     experiment.log_parameter("model", args.model)
-    gen_train, gen_val, gen_test = utils.setup_generators(experiment, data_path)
     experiment.log_parameter("epochs", 10)
+    gen_train, gen_val, gen_test = utils.setup_generators(experiment, args.task)
 
     # Build the model:
     if (args.model == "srresnet"):
@@ -44,7 +35,11 @@ for experiment in opt.get_experiments():
     else:
         raise Exception("Unknown model")
 
-    models.SRResNet.train(experiment, model, gen_train, gen_val)
+    # Train the model:
+    if (args.model == "srresnet"):
+        models.SRResNet.train(experiment, model, gen_train, gen_val)
+    else:
+        raise Exception("Unknown model")
 
     # How well did it do?
     utils.plot_results(experiment, model, gen_val)
